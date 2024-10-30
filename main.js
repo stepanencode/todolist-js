e0e59ea2d3eacd22cc46c93464e2d43b81eff19a
Oksana Stepanenko <stepanencode@gmail.com>
2024-10-30 14:05:25 +0100

    add render list

--- main.js
+++ main.js
@@ -4,11 +4,9 @@ let taskId = 1;
 const todoList = document.getElementById('todo-list');
 const form = document.getElementById('todo-form');
 
-// create DOM nodes
 function createListItemUI(task) {
   const li = document.createElement('li');
   li.setAttribute('data-id', task.id);
-
   return li;
 }
 
@@ -16,28 +14,24 @@ function createDoneMarkerUI(task) {
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.checked = task.isChecked;
-
   return checkbox;
 }
 
 function createTaskTextUI(task) {
   const span = document.createElement('span');
   span.textContent = task.text;
-
   return span;
 }
 
 function createTaskButtonsUI() {
   const taskButtons = document.createElement('div');
   taskButtons.className = 'task-buttons';
-
   return taskButtons;
 }
 
 function createListItemContainerUI() {
   const taskName = document.createElement('div');
   taskName.className = 'task-name';
-
   return taskName;
 }
 
@@ -45,39 +39,39 @@ function createDeleteButtonUI() {
   const deleteButton = document.createElement('button');
   deleteButton.className = 'delete';
   deleteButton.textContent = 'Delete';
-
   return deleteButton;
 }
 
-// actions with DOM
-function addTaskToDOM(task) {
+function createTaskElement(task) {
   const container = createListItemUI(task);
   const itemContainer = createListItemContainerUI();
-  const text = createTaskTextUI(task);
   const marker = createDoneMarkerUI(task);
+  const text = createTaskTextUI(task);
   const buttonContainer = createTaskButtonsUI();
   const deleteButton = createDeleteButtonUI();
 
-  container.appendChild(itemContainer);
+  // Сборка элементов задачи
   itemContainer.appendChild(marker);
   itemContainer.appendChild(text);
-  container.appendChild(buttonContainer);
+  container.appendChild(itemContainer);
   buttonContainer.appendChild(deleteButton);
-  todoList.appendChild(container);
+  container.appendChild(buttonContainer);
+
+  return container;
 }
 
-function removeTaskFromDOM(taskId) {
-  const taskElement = document.querySelector(`li[data-id='${taskId}']`);
-  if (taskElement) {
-    taskElement.remove();
-  }
+function renderList() {
+  todoList.innerHTML = '';
+
+  tasksList.forEach((task) => {
+    const taskElement = createTaskElement(task);
+    todoList.appendChild(taskElement);
+  });
 }
 
-// actions with state
 function getInputFieldValue() {
   const input = document.getElementById('todo-input');
   const taskText = input.value.trim();
-
   return taskText === '' ? null : taskText;
 }
 
@@ -88,7 +82,6 @@ function addTaskToList(taskText) {
     id: taskId++,
   };
   tasksList.push(newTask);
-
   return newTask;
 }
 
@@ -96,28 +89,23 @@ function removeTaskFromList(taskId) {
   tasksList = tasksList.filter((task) => task.id !== Number(taskId));
 }
 
-// handle events
 function handleAddTask(event) {
   event.preventDefault();
-
   const taskText = getInputFieldValue();
   if (!taskText) return;
 
-  const newTask = addTaskToList(taskText);
-  addTaskToDOM(newTask);
-
+  addTaskToList(taskText);
+  renderList();
   form.reset();
 }
 
 function handleRemoveTask(event) {
   if (event.target.classList.contains('delete')) {
     const taskId = event.target.closest('li').getAttribute('data-id');
-
     removeTaskFromList(taskId);
-    removeTaskFromDOM(taskId);
+    renderList();
   }
 }
 
-// add event listeners
 form.addEventListener('submit', handleAddTask);
 todoList.addEventListener('click', handleRemoveTask);
